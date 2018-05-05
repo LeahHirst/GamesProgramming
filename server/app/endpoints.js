bf5e3751312ad850384b9b2c276bedc9bcdb541b
Adam Hirst <adam@ahirst.com>
2018-05-05 22:03:37 +0100

    [Server] Added host and join game

--- server/app/endpoints.js
+++ server/app/endpoints.js
@@ -1,5 +1,6 @@
 // Import config
 const config  = require(process.env.DD_CONFIG || '/etc/dd_conf/config.json');
+const rand    = require('./rand');
 
 // Export func
 module.exports = (io) => { 
@@ -43,6 +44,20 @@ module.exports = (io) => {
         }
     }
 
+    function addGame(gameId) {
+
+    }
+
+    function joinGame(gameId, socket) {
+        // Return false if the game does not exist
+        if (game[gameId] == undefined) return false;
+        // Join the room corrosponsind to the game ID
+        socket.join(gameId);
+        // Add the user to the room
+        games[gameId].users[socket.id] = users[socket.id];
+        return true;
+    }
+
     /**
      * Handler on a socket connection being a established
      */
@@ -72,20 +87,43 @@ module.exports = (io) => {
         /**
          * Join game request
          */
-        socket.on('join game', (gameId) => {
+        socket.on('join game', (gameId, cb) => {
             log(socket.id, `Game join request. Game ID: ${gameId}`);
-            socket.join(gameId);
             
-            // Check if the joined room is empty
+            // Attempt to join the game
+            cb(joinGame(gameId));
         });
 
         /**
          * Host game request
          */
-        socket.on('host game', () => {
+        socket.on('host game', (cb) => {
             log(socket.id, `Host game request`);
             
-            // TODO: Generate room ID, join room.
+            // Generate a Game ID
+            var gId = rand.genereteRandomId(config.GAME_ID_LENGTH);
+            while (games[gId] != undefined) {
+                gId = rand.genereteRandomId(config.GAME_ID_LENGTH);
+            }
+
+            // Setup the game
+            games[gId] = {
+                users = {},
+                objects = []
+            };
+
+            // Join the game
+            joinGame(gId);
+
+            // Return the game ID
+            cb(gId);
+        });
+
+        /**
+         * Leave game request
+         */
+        socket.on('leave game', () => {
+            log(socket.id, `Game leave request`);
         });
 
         /**
