c3b199d37595668dab42f67b2a901484567a8d62
Adam Hirst <adam@ahirst.com>
2018-05-05 21:34:11 +0100

    [Server] Removed redundant config artifacts

--- server/index.js
+++ server/index.js
@@ -7,31 +7,16 @@
  *  /_____/\____/\____/\__,_/\__,_/\__,_/  /_____/\__,_/____/_/ /_/ 
  *                                                               
  * ==================================================================
- *    Doodad Dash ~ v1.0.0 ~ (c) Adam Hirst 2018 ~ adam@ahirst.com
+ *    Doodad Dash ~ v1.0.0 ~ (c) Adam Hirst 2018 ~ adam@ahirst.com   
  * ==================================================================
  */
-
-// Internal debug flag
-const DEBUG = true;
 
 // Dependencies
 const fs = require('fs');
 const GameServerFactory = require('./app/gameserver');
 
-// HTTPS credentials
-var httpsCredentials;
-if (!!process.env.DDD_KEY && !!process.env.DDD_KEY) {
-    httpsCredentials = {
-        key: fs.readFileSync(process.env.DDD_KEY),
-        cert: fs.readFileSync(process.env.DDD_CERT),
-    }
-}
-
 // Create the game server
-const gs = GameServerFactory.createGameServer({
-    debug: DEBUG,
-    https: httpsCredentials
-});
+const gs = GameServerFactory.createGameServer();
 
 // Listen on port 3000
 gs.listen(3000, () => {
