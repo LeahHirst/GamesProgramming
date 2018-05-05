1656e57c6fc65fd6aa64ebac2bed531caf776f8a
Adam Hirst <adam@ahirst.com>
2018-05-05 21:34:32 +0100

    [Server] Updated GameServer for new config setup

--- server/app/gameserver.js
+++ server/app/gameserver.js
@@ -4,35 +4,40 @@
 
 const express = require('express');
 const app     = express();
-const sio     = require('socket.io')
+const sio     = require('socket.io');
+const config  = require(process.env.DD_CONFIG || '/etc/dd_conf/config.json');
 
+app.use(express.static('public'));
+
+/**
+ * Game Server class holds global game states
+ */
 class GameServer {
 
     /**
      * GameServer constructor
-     * @param {Object} opts 
      */
-    constructor (opts) {
-        if (!!opts.debug) {
-
+    constructor () {
+        if (config.DEBUG) {
             // Start in debug mode
             this.debug = true;
 
             // Create server
             this.server = require('http').createServer(app);
-
         } else {
-
             // Start as production
             this.debug = false;
 
             // Create server
-            if (!opts.https) throw new Error('HTTPS required for production');
-            this.server = require('https').createServer(opts.https);
-
+            if (!config.HTTPS) throw new Error('HTTPS required for production');
+            this.server = require('https').createServer(config.HTTPS);
         }
 
+        // Initiate a socket.io instance
         this.io = sio(this.server);
+
+        // Setup socket.io endpoints
+        require('./endpoints')(this.io);
     }
 
     /**
@@ -50,8 +55,8 @@ class GameServer {
  * Creates a new instance of a GameServer.
  * @param {Object} opts 
  */
-function createGameServer(opts) {
-    return new GameServer(opts);
+function createGameServer() {
+    return new GameServer();
 }
 
 // Expose createGameServer to other modules
