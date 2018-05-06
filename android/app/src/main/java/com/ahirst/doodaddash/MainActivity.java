715ec7e8c67ed6c62feeec51fed1adc392319b99
Adam Hirst <adam@ahirst.com>
2018-05-06 17:52:24 +0100

    [Android] Started main menu layout

--- android/app/src/main/java/com/ahirst/doodaddash/MainActivity.java
+++ android/app/src/main/java/com/ahirst/doodaddash/MainActivity.java
@@ -7,6 +7,8 @@ import android.support.v4.app.ActivityCompat;
 import android.support.v4.content.ContextCompat;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.Window;
+import android.view.WindowManager;
 
 import com.google.android.cameraview.CameraView;
 
@@ -20,6 +22,8 @@ public class MainActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
 
+        requestWindowFeature(Window.FEATURE_NO_TITLE);
+        getWindow().setFlags(WindowManager.LayoutParams.FLAG_FULLSCREEN, WindowManager.LayoutParams.FLAG_FULLSCREEN);
         setContentView(R.layout.activity_main);
 
         mCameraView = (CameraView) findViewById(R.id.camera);
